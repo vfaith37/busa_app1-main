552ac6861f626781eff85eaebbd9711dbe1372fb
ourgithubacc <dada.stephenolamide@gmail.com>
2023-03-08 09:04:01 +0100

    Revert "new commits"
    This reverts commit 0cb8491befd1e80f4b9223de4f8422d7a54d38d1.

--- api/client.js
+++ api/client.js
@@ -1,8 +1,14 @@
 import axios from 'axios';
+// import { setupCache } from 'axios-cache-adapter';
 
+// Create a cache adapter with a TTL of 5 minutes
+// const cache = setupCache({
+//   maxAge: 5 * 60 * 1000,
+// });
 
 export default axios.create(
     { baseURL: 'https://finalissima.onrender.com/api' , 
+    // adapter: cache.adapter,
 },
     
 
