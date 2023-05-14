be5af2984c036fa9df29a014a2b0b97782b829da
Light <lightofthewoorlddd@gmail.com>
2023-05-14 12:23:06 +0100

    Update FormInput.js

--- Components/FormInput.js
+++ Components/FormInput.js
@@ -1,23 +1,45 @@
-import { Text, TextInput, View } from "react-native";
+import { Text, TextInput, View, TouchableOpacity } from "react-native";
 import React from "react";
-
 export const FormInput = (props) => {
-	const { label, error, style, TextInputStyle } = props;
+	const { label, error, style, TextInputStyle, rightIcon, onPress } = props;
 	return (
 		<>
-			<View style={{ flexDirection: "row", justifyContent: "space-between", paddingTop: 20  }}>
+			<View
+				style={{
+					flexDirection: "row",
+					justifyContent: "space-between",
+					paddingTop: 20,
+				}}
+			>
 				<Text style={style}>{label}</Text>
 				{error ? (
-					<Text style={{ fontSize: 11, color: "red", fontFamily:"Poppins"}}>{error}</Text>
+					<Text style={{ fontSize: 11, color: "red", fontFamily: "Poppins" }}>
+						{error}
+					</Text>
 				) : null}
 			</View>
-			<TextInput
-				{...props}
-				style={TextInputStyle}
-				selectionColor={'#363BE8'}
-				cursorColor={"#363be8"}
-			/>
+			<View>
+				<TextInput
+					{...props}
+					style={TextInputStyle}
+					selectionColor={"#363BE8"}
+					cursorColor={"#363be8"}
+				/>
+				{rightIcon && (
+					<TouchableOpacity
+						onPress={onPress}
+						style={
+							{
+								position: "absolute",
+								right: 10,
+								top: 8,
+							}
+						}
+					>
+						{rightIcon}
+					</TouchableOpacity>
+				)}
+			</View>
 		</>
 	);
 };
-
